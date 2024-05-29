3a53c734b8f2ca137ce73ee27eebe18500c0b03e
Emilio Luna Jandar <lunaemilio269@gmail.com>
2024-05-29 01:43:54 -0300

    Footer.jsx was added, with its respective modeling and with the option to invert the colors

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "./styles/main.css";
 import "./styles/navFooter.css";
 import Navbar from "./layout/Navbar";
+import Footer from "./layout/Footer"
 
 function App() {
 
   return (
     <>
       <Navbar/>
+      <Footer/>
     </>
   )
 }
